6ffe48a5161718231310c09b206b2293ca59a6da
sabrinaachille <92241507+sabrinaachille@users.noreply.github.com>
2024-09-23 14:24:26 -0400

    Update Staff.java
    TODO 1, added 6 new StaffEmployees, done.

--- src/main/java/com/mycompany/agency/Staff.java
+++ src/main/java/com/mycompany/agency/Staff.java
@@ -14,9 +14,14 @@ public class Staff
     {
         staffList = new StaffMember[6];
 
-        // TODO 1: Add 6 emploees to the staffList
+        // TODO 1: Add 6 emploees to the staffList DONE - Sabrina.
 
         staffList[0] = new StaffEmployee("Bobby","123 Fake Street","(847) 625-7272","222-22-2222",45.2);
+        staffList[1] = new StaffEmployee("Ferb","456 Fake Street","(646) 741-8520","111-11-1111",25.6);
+        staffList[2] = new StaffEmployee("Sandy","789 Fake Street","(929) 963-9635","333-33-3333",35.5);
+        staffList[3] = new StaffEmployee("Darwin","357 Fake Street","(646) 258-8456","444-44-4444",23.0);
+        staffList[4] = new StaffEmployee("Mable","951 Fake Street","(847) 485-1236","666-66-6666",47.2);
+        staffList[5] = new StaffEmployee("Lilly","852 Fake Street","(929) 621-7372","555-55-5555",23.0);
 
 //        ((Executive)staffList[0]).awardBonus(500.00);
 //
